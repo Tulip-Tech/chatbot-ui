258b2b4d02fb550ed57c2232a63346d0e63a2508
mmarifat <mma.rifat66@gmail.com>
2024-07-10 15:34:08 +0100

    chore: window.location.href to login page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,7 +3,6 @@
 import { ChatbotUISVG } from "@/components/icons/chatbotui-svg"
 import { IconArrowRight } from "@tabler/icons-react"
 import { useTheme } from "next-themes"
-import Link from "next/link"
 
 export default function HomePage() {
   const { theme } = useTheme()
@@ -16,13 +15,13 @@ export default function HomePage() {
 
       <div className="mt-2 text-4xl font-bold">Chatbot UI</div>
 
-      <Link
-        className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
-        href="/login"
+      <div
+        className="mt-4 flex w-[200px] cursor-pointer items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
+        onClick={() => (window.location.href = "/login")}
       >
         Start Chatting
         <IconArrowRight className="ml-1" size={20} />
-      </Link>
+      </div>
     </div>
   )
 }
